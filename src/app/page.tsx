a832694e642d61b5194434e751babd85ac9ef296
its-rayan <rayanmabdirahman@gmail.com>
2025-06-03 22:53:25 +0100

    config Nextjs and Shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>hello world</h1>
+        <Button>Click me</Button>
+      </main>
+    </div>
+  );
+}
