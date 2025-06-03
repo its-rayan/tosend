b555fcd9f5feabdd0e6086df2a5bee2fe1fba95b
its-rayan <rayanmabdirahman@gmail.com>
2025-06-03 22:25:25 +0100

    delete turborepo

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <main className="mx-auto w-full px-3 relative max-w-screen-lg lg:px-4 xl:px-0">
-        <h1>hello world</h1>
-      </main>
-    </div>
-  );
-}
--- packages/ui/src/index.ts
+++
@@ -1,5 +0,0 @@
-// styles
-import "./styles/globals.css";
-
-// components
-export { Button } from "./components/ui/button";
