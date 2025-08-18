6cd6427ab3b471decd0ea8672d1f5e8cb47f64e8
its-rayan <rayanmabdirahman@gmail.com>
2025-08-19 00:01:38 +0100

    feat: add mongodb adapter to  AuthJS

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,10 @@
+import connectToMongoClient from '@/database/connect-mongo-client';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
+
+const mongoClient = connectToMongoClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [],
+  adapter: MongoDBAdapter(mongoClient),
 });
---
+++ src/database/connect-mongo-client.ts
@@ -0,0 +1,45 @@
+/*################################################
+###### NEEDED TO CONNECT MONGODB TO AUTHJS  ######
+#################################################*/
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+type GlobalThis = typeof globalThis & {
+  _mongoClient?: Promise<MongoClient>;
+};
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const mongoClientOptions = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
+
+export default function connectToMongoClient() {
+  // Check for required environment variables
+  if (!MONGO_URI) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+  }
+
+  // If we are in development, use the global promise
+  if (process.env.NODE_ENV === 'development') {
+    const globalWithMongo = global as GlobalThis;
+    globalWithMongo._mongoClient = undefined;
+
+    // If the promise is still undefined, create a new MongoClient
+    if (!globalWithMongo._mongoClient) {
+      globalWithMongo._mongoClient = new MongoClient(
+        MONGO_URI,
+        mongoClientOptions,
+      ).connect();
+    }
+
+    return globalWithMongo._mongoClient;
+  }
+
+  // If we are not in development, create a new MongoClient
+  return new MongoClient(MONGO_URI, mongoClientOptions).connect();
+}
