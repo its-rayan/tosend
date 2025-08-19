bc11cd69caa2fed7428a0de85eed37414917edd1
its-rayan <rayanmabdirahman@gmail.com>
2025-08-20 00:44:32 +0100

    feat: add resend provider for magic link

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,17 @@
 import connectToMongoClient from '@/database/connect-mongo-client';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
+import Resend from 'next-auth/providers/resend';
 
 const mongoClient = connectToMongoClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Resend({
+      apiKey: process.env.RESEND_API_KEY,
+      from: 'no-reply@resend.tosend.co',
+      name: 'Email',
+    }),
+  ],
   adapter: MongoDBAdapter(mongoClient),
 });
