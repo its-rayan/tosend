9db077f9ae3cf7f435fb4722f435f6d81c7641c7
its-rayan <rayanmabdirahman@gmail.com>
2025-08-20 01:38:23 +0100

    feat: configure google oauth

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import connectToMongoClient from '@/database/connect-mongo-client';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
 import Resend from 'next-auth/providers/resend';
 
 const mongoClient = connectToMongoClient();
@@ -11,6 +12,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       apiKey: process.env.RESEND_API_KEY,
       from: 'no-reply@resend.tosend.co',
       name: 'Email',
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(mongoClient),
