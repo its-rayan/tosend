3ff6bce16bacb6001a2b7a17cdff24102c8bb8ef
its-rayan <rayanmabdirahman@gmail.com>
2025-08-21 22:49:30 +0100

    feat: use custom form for email sign in

--- src/components/auth/signin/email-signin.tsx
+++ src/components/auth/signin/email-signin.tsx
@@ -1,18 +1,45 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { signIn } from 'next-auth/react';
+import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function SignInForm() {
+  const [isPending, setIsPending] = useState(false);
+  const form = useForm<{ email: string }>({
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const { register, getValues } = form;
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    setIsPending(true);
+    e.preventDefault();
+    const { email } = getValues();
+    await signIn('resend', { email, callbackUrl: '/workspace' });
+    setIsPending(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="grid gap-6">
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input
+            id="email"
+            type="email"
+            placeholder="m@example.com"
+            required
+            {...register('email')}
+          />
         </div>
 
-        <Button type="submit" className="w-full">
-          Login
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? 'Submitting...' : 'Login'}
         </Button>
       </div>
     </form>
