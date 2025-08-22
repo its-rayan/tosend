26f796875a158c07f1eecf0d44213bcfca300f4f
its-rayan <rayanmabdirahman@gmail.com>
2025-08-22 23:46:58 +0100

    feat: move auth providers to config file and add middleware

--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,15 @@
+import authConfig from '@/auth.config';
 import connectToMongoClient from '@/database/connect-mongo-client';
-import { AUTH_FROM_EMAIL, AUTH_PAGES } from '@/util/constants/auth';
+import { AUTH_PAGES } from '@/util/constants/auth';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
-import Resend from 'next-auth/providers/resend';
 
 const mongoClient = connectToMongoClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Resend({
-      apiKey: process.env.RESEND_API_KEY,
-      from: AUTH_FROM_EMAIL,
-    }),
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
   adapter: MongoDBAdapter(mongoClient),
+  session: { strategy: 'jwt' },
+  ...authConfig,
   pages: {
     signIn: AUTH_PAGES.SIGN_IN,
     newUser: AUTH_PAGES.SIGN_UP,
---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import authConfig from '@/auth.config';
+import NextAuth from 'next-auth';
+import type { NextRequest } from 'next/server';
+const { auth } = NextAuth(authConfig);
+
+export default auth(async function middleware(req: NextRequest) {
+  // Your custom middleware logic goes here
+  console.log('ROUTES: ', req.nextUrl.pathname);
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
