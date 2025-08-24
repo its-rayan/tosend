65f20cb1f19321e94be24a1a323fa82b9aa47b0a
its-rayan <rayanmabdirahman@gmail.com>
2025-08-24 13:36:54 +0100

    feat: globally cache mongoose connection

--- src/database/connect-db-client.ts
+++ src/database/connect-db-client.ts
@@ -4,6 +4,17 @@
 
 import mongoose from 'mongoose';
 
+type cachedMongoose = {
+  conn: typeof mongoose | null;
+  promise: typeof mongoose | null;
+};
+
+type GlobalThis = typeof globalThis & {
+  mongoose: cachedMongoose;
+};
+
+const globalWithMongoose = global as GlobalThis;
+
 const MONGO_URI = process.env.MONGO_URI;
 
 async function connectToDbClient() {
@@ -12,8 +23,25 @@ async function connectToDbClient() {
       throw new Error('Invalid/Missing environment variable: "MONGO_URI"');
     }
 
-    await mongoose.connect(MONGO_URI);
-    console.log('✅ Successfully connected to database');
+    // check if mongoose connection is already cached
+    let cached = globalWithMongoose.mongoose;
+    if (!cached) {
+      cached = globalWithMongoose.mongoose = { conn: null, promise: null };
+    }
+
+    if (cached.conn) {
+      console.log('Cached mongoose connection is called!');
+      return cached.conn;
+    }
+
+    if (!cached.promise) {
+      mongoose.set('strictQuery', true);
+      cached.promise = await mongoose.connect(MONGO_URI);
+      console.log('✅ Successfully connected to database');
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
   } catch (error) {
     console.error('🚨 Failed to connect to database: ', error);
     process.exit(1);
