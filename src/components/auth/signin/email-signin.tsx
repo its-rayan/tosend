78d107a416277e801b0837133935825f0752f6ed
its-rayan <rayanmabdirahman@gmail.com>
2025-08-24 16:25:06 +0100

    feat: add server action to prevent new users from signing in

--- src/components/auth/signin/email-signin.tsx
+++ src/components/auth/signin/email-signin.tsx
@@ -3,12 +3,20 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { checkAccountExistsAction } from '@/lib/actions/auth/check-account-exists';
 import { signIn } from 'next-auth/react';
-import { FormEvent, useState } from 'react';
+import { useAction } from 'next-safe-action/hooks';
+import { FormEvent } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function SignInForm() {
-  const [isPending, setIsPending] = useState(false);
+  const { executeAsync, isPending } = useAction(checkAccountExistsAction, {
+    onError: ({ error }) => {
+      console.error('Error checking account existence:', error);
+      alert(error.serverError);
+    },
+  });
+
   const form = useForm<{ email: string }>({
     defaultValues: {
       email: '',
@@ -16,13 +24,23 @@ export default function SignInForm() {
   });
 
   const { register, getValues } = form;
+
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    setIsPending(true);
     e.preventDefault();
+
     const { email } = getValues();
-    // await signIn('resend', { email, redirectTo: '/workspace' });
+
+    const result = await executeAsync({ email });
+    if (!result?.data) return;
+
+    // check if account exists before sending magic link
+    const { accountExists } = result.data;
+    if (!accountExists) {
+      alert('No account found with that email address.');
+      return;
+    }
+
     await signIn('nodemailer', { email, redirectTo: '/workspace' });
-    setIsPending(false);
   };
 
   return (
