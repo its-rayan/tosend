d65818222c81be34b50586cbd9dba05533759730
its-rayan <rayanmabdirahman@gmail.com>
2025-08-27 11:49:49 +0100

    feat: add sesson lib file to get current user

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,13 +1,13 @@
-import { auth } from '@/auth';
 import SignOutButton from '@/components/auth/signout-button';
+import { getCurrentUser } from '@/lib/auth/session';
 
 export default async function WorkspacePage() {
-  const session = await auth();
+  const user = await getCurrentUser();
   return (
     <div className="flex items-center gap-8">
       <div>
         <h1>Workspace</h1>
-        {JSON.stringify(session)}
+        {JSON.stringify(user)}
       </div>
       <SignOutButton />
     </div>
